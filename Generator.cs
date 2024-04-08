db464913cdcebf7678c04b8c4c5d53d143f71ffb
DE-YU_H14 <delsin_yu@qq.com>
2024-04-08 09:58:00 +0800

    Change target product from editor addon to standalone applicateion

--- Generator.cs
+++ Generator.cs
@@ -1,33 +1,28 @@
-﻿#if TOOLS
-
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using Godot;
 using Godot.BindingsGenerator.ApiDump;
-using Environment = System.Environment;
 
 namespace GDExtensionAPIGenerator;
 
 internal static class Generator
 {
-    public static void Generate()
+    public static void Generate(string godotPath, string projectPath)
     {
         GD.Print($"""
                  Dumping Godot Builtin Classes...
-                 Starting Godot Editor ({Path.GetFileName(Environment.ProcessPath)})
+                 Starting Godot Editor ({godotPath})
                  
-                 Command Line: {Environment.ProcessPath} --dump-extension-api --verbose --headless
+                 Command Line: {godotPath} --dump-extension-api --verbose --headless
                  
                  -----------------------Godot Message Start------------------------
                  """
                  );
         var dumpJsonCoreStartInfo =
             new ProcessStartInfo(
-                Environment.ProcessPath!,
+                godotPath,
                 "--dump-extension-api --verbose --headless"
             ) { RedirectStandardOutput = true };
         
@@ -44,9 +39,9 @@ internal static class Generator
                   Finish Collecting Builtin Classes.
 
                   Dumping Godot Builtin Classes With GDExtensions...
-                  Starting Godot Editor ({Path.GetFileName(Environment.ProcessPath)})
-                  With Current Project Root ({Path.GetFullPath("./")})
-                  Command Line: {Environment.ProcessPath} res://addons/cs_wrapper_generator_for_gde/empty_scene.tscn --path {Path.GetFullPath("./")} --dump-extension-api --headless --verbose --editor
+                  Starting Godot Editor ({godotPath})
+                  With Current Project Root ({projectPath})
+                  Command Line: {godotPath} res://addons/cs_wrapper_generator_for_gde/empty_scene.tscn --path {projectPath} --dump-extension-api --headless --verbose --editor
                   
                   -----------------------Godot Message Start------------------------
                   """
@@ -55,9 +50,9 @@ internal static class Generator
         GD.Print("Dumping Godot Engine Classes With GDExtension...");
         var dumpJsonGDEStartInfo =
             new ProcessStartInfo(
-                Environment.ProcessPath!,
+                godotPath,
                 $"res://addons/cs_wrapper_generator_for_gde/empty_scene.tscn" +
-                $" --path {Path.GetFullPath("./")}" +
+                $" --path {projectPath}" +
                 $" --dump-extension-api" +
                 $" --headless" +
                 $" --verbose" +
@@ -80,8 +75,8 @@ internal static class Generator
         
         GD.Print($"""
                  Classes in extension_api.json from:
-                 Godot Editor ({Path.GetFileName(Environment.ProcessPath)}): {godotBuiltinClass.Count}
-                 Current Project ({Path.GetDirectoryName(Path.GetFullPath("./"))}): {godotGDEClasses.Count}
+                 Godot Editor ({Path.GetFileName(godotPath)}): {godotBuiltinClass.Count}
+                 Current Project ({Path.GetDirectoryName(projectPath)}): {godotGDEClasses.Count}
                  Class Count in ClassDB {ClassDB.GetClassList().Length}
                  """
                  );
@@ -96,13 +91,4 @@ internal static class Generator
         File.Delete(extensionPath);
         return dictionary;
     }
-
-    public static void UnloadSystemTextJson()
-    {
-        var assembly = typeof(JsonSerializerOptions).Assembly;
-        var updateHandlerType = assembly.GetType("System.Text.Json.JsonSerializerOptionsUpdateHandler");
-        var clearCacheMethod = updateHandlerType?.GetMethod("ClearCache", BindingFlags.Static | BindingFlags.Public);
-        clearCacheMethod?.Invoke(null, new object[] { null }); 
-    }
 }
-#endif
