a800828c06c4327a5804e42e99740b3f4bb2379c
DE-YU_H14 <delsin_yu@qq.com>
2024-04-11 09:42:39 +0800

    Code cleanup

--- addons/cs_wrapper_generator_for_gde/Generator.cs
+++ addons/cs_wrapper_generator_for_gde/Generator.cs
@@ -1,6 +1,7 @@
 ﻿#if TOOLS
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -14,44 +15,127 @@ namespace GDExtensionAPIGenerator;
 
 internal static partial class Generator
 {
-    static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
-    {
-        // Get information about the source directory
-        var dir = new DirectoryInfo(sourceDir);
-
-        // Check if the source directory exists
-        if (!dir.Exists)
-            throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
-
-        // Cache directories before we start copying
-        DirectoryInfo[] dirs = dir.GetDirectories();
-
-        // Create the destination directory
-        Directory.CreateDirectory(destinationDir);
-
-        // Get the files in the source directory and copy to the destination directory
-        foreach (FileInfo file in dir.GetFiles())
-        {
-            string targetFilePath = Path.Combine(destinationDir, file.Name);
-            file.CopyTo(targetFilePath);
-        }
-
-        // If recursive and copying subdirectories, recursively call this method
-        if (recursive)
-        {
-            foreach (DirectoryInfo subDir in dirs)
-            {
-                string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
-                CopyDirectory(subDir.FullName, newDestinationDir, true);
-            }
-        }
-    }
-
-    [GeneratedRegex(@"WRAPPER_GENERATOR_DUMP_CLASS_DB_START(?<ClassNames>.+?)WRAPPER_GENERATOR_DUMP_CLASS_DB_END",
-        RegexOptions.Singleline | RegexOptions.NonBacktracking)]
+    [GeneratedRegex(
+        "WRAPPER_GENERATOR_DUMP_CLASS_DB_START(?<ClassNames>.+?)WRAPPER_GENERATOR_DUMP_CLASS_DB_END",
+        RegexOptions.Singleline | RegexOptions.NonBacktracking
+    )]
     private static partial Regex GetExtractClassNameRegex();
 
     public static void Generate()
+    {
+        var tempPath = CreateTempDirectory();
+        var scriptFullPath = CreateDumpDBScript(tempPath);
+        var dummyProjectPath = CreateDummyProject(tempPath);
+        var godotExecutablePath = CopyGodotExecutable(Environment.ProcessPath!, tempPath);
+        string[] dumpGodotClassCommands = ["--headless", "--script", scriptFullPath, "--editor", "--verbose", "--path", dummyProjectPath];
+
+        GD.Print(
+            $"""
+             Dumping Godot Builtin Classes...
+             Starting Godot Editor ({Path.GetFileName(Environment.ProcessPath)})
+             Command Line: {godotExecutablePath} {string.Join(' ', dumpGodotClassCommands)}
+             """
+        );
+
+        string resultString;
+        using (var result = new Godot.Collections.Array())
+        {
+            OS.Execute(godotExecutablePath, dumpGodotClassCommands, result);
+            Directory.Delete(tempPath, true);
+            try
+            {
+                resultString = result[0].AsString();
+            }
+            catch (Exception e)
+            {
+                GD.PrintErr(e.ToString());
+                return;
+            }
+        }
+
+        if (ExtractClassNamesFromStdOut(resultString, out var builtinClassTypes)) return;
+
+        var currentClassTypes = ClassDB.GetClassList();
+
+        var filteredTypes = currentClassTypes
+            .Except(builtinClassTypes)
+            .Select(x => (StringName)x)
+            .Where(ClassDB.CanInstantiate)
+            .ToHashSet();
+
+        foreach (var gdeClassName in filteredTypes)
+        {
+            GD.Print($"ClassNames:{gdeClassName} IsClassEnabled:{ClassDB.IsClassEnabled(gdeClassName)} GetParentClass:{ClassDB.GetParentClass(gdeClassName)}");
+        }
+
+        GD.Print($"currentClassTypes:{currentClassTypes.Length} builtinClassTypes:{builtinClassTypes.Count} diffType:{filteredTypes.Count}");
+
+        //EditorInterface.Singleton.RestartEditor(true);
+    }
+
+    private static bool ExtractClassNamesFromStdOut(string resultString, out HashSet<string> builtinClassTypes)
+    {
+        var matchResult = GetExtractClassNameRegex().Match(resultString);
+        if (!matchResult.Success)
+        {
+            builtinClassTypes = null;
+            return true;
+        }
+
+        builtinClassTypes = matchResult
+            .Groups["ClassNames"]
+            .Value
+            .Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .ToHashSet();
+        return false;
+    }
+
+    private static string CopyGodotExecutable(string godotExecutablePath, string tempPath)
+    {
+        var godotExecutableDir = Path.GetDirectoryName(godotExecutablePath)!;
+        var allGodotExecutableFiles = Directory.GetFiles(godotExecutableDir, "*.*", SearchOption.AllDirectories);
+        var copyTasks = new Task[allGodotExecutableFiles.Length];
+        for (var i = 0; i < allGodotExecutableFiles.Length; i++)
+        {
+            var sourcePath = allGodotExecutableFiles[i];
+            copyTasks[i] = Task.Run(
+                () =>
+                {
+                    var relativePath = Path.GetRelativePath(godotExecutableDir, sourcePath);
+                    var destPath = Path.Combine(tempPath, relativePath);
+                    var destDir = Path.GetDirectoryName(destPath);
+                    if (!Directory.Exists(destDir))
+                    {
+#if GODOT_OSX || GODOT_LINUXBSD
+                        Directory.CreateDirectory(destDir, UnixFileMode.UserExecute);
+#else
+                        Directory.CreateDirectory(destDir);
+#endif
+                    }
+
+                    File.Copy(sourcePath, destPath);
+                }
+            );
+        }
+
+        Task.WhenAll(copyTasks).Wait();
+        return Path.Combine(tempPath, Path.GetFileName(godotExecutablePath));
+    }
+
+    private static string CreateDummyProject(string tempPath)
+    {
+        var dummyProjectPath = Path.Combine(tempPath, "project");
+        Directory.CreateDirectory(dummyProjectPath);
+        using var config = new ConfigFile();
+        config.SetValue(string.Empty, "config_version", 5);
+        config.SetValue("application", "config/features", ProjectSettings.GetSetting("config/features"));
+        config.SetValue("application", "config/name", "Empty Project");
+        File.WriteAllText(Path.Combine(dummyProjectPath, "project.godot"), config.ToString());
+
+        return dummyProjectPath;
+    }
+
+    private static string CreateDumpDBScript(string tempPath)
     {
         const string dumpDBScript =
             """
@@ -67,75 +151,17 @@ internal static partial class Generator
             	
             """;
         const string dumpDBFileName = "dump_class_db.gd";
-
-        var tempPath = Path.GetTempFileName();
-
-        File.Delete(tempPath);
-
-        Directory.CreateDirectory(tempPath);
-
         var scriptFullPath = Path.Combine(tempPath, dumpDBFileName);
-
         File.WriteAllText(scriptFullPath, dumpDBScript);
+        return scriptFullPath;
+    }
 
-        var dumpGodotClassProcess = Environment.ProcessPath!;
-        var newDumpGodotClassProcess = Path.Combine(tempPath, Path.GetFileName(dumpGodotClassProcess)!);
-        var tempProjectPath = Path.Combine(tempPath, "project");
-        Directory.CreateDirectory(tempProjectPath);
-        var config = new ConfigFile();
-        config.SetValue("", "config_version", 5);
-        config.SetValue("application", "config/features", ProjectSettings.GetSetting("config/features"));
-        config.SetValue("application", "config/name", "Empty Project");
-        File.WriteAllText(Path.Combine(tempProjectPath, "project.godot"), config.ToString());
-        config.Dispose();
-        CopyDirectory(Path.GetDirectoryName(dumpGodotClassProcess), tempPath, true);
-        dumpGodotClassProcess = newDumpGodotClassProcess;
-        string[] dumpGodotClassCommands =
-            ["--headless", "--script", scriptFullPath, "--editor", "-v", $"--path \"{tempProjectPath}\""];
-
-        GD.Print($"""
-                  Dumping Godot Builtin Classes...
-                  Starting Godot Editor ({Path.GetFileName(Environment.ProcessPath)})
-                  Command Line: {dumpGodotClassProcess} {string.Join(' ', dumpGodotClassCommands)}
-                  """
-        );
-
-        string resultString;
-        {
-            using var result = new Godot.Collections.Array();
-            OS.Execute(dumpGodotClassProcess, dumpGodotClassCommands, result);
-            Directory.Delete(tempPath, true);
-            try
-            {
-                resultString = result[0].AsString();
-            }
-            catch (Exception e)
-            {
-                GD.PrintErr(e.ToString());
-                return;
-            }
-        }
-
-        var matchResult = GetExtractClassNameRegex().Match(resultString);
-
-        if (!matchResult.Success) return;
-
-        var builtinClassTypes = matchResult
-            .Groups["ClassNames"]
-            .Value
-            .Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
-            .ToHashSet();
-
-
-        var currentClassTypes = ClassDB.GetClassList();
-        var diffType = currentClassTypes.Except(builtinClassTypes).Where(x => ClassDB.CanInstantiate(x)).ToHashSet();
-        foreach (var gdeClassNames in diffType)
-        {
-            GD.Print($"ClassNames:{gdeClassNames} IsClassEnabled:{ClassDB.IsClassEnabled(gdeClassNames)} GetParentClass:{ClassDB.GetParentClass(gdeClassNames)}");
-        }
-
-        GD.Print($"currentClassTypes:{currentClassTypes.Length} builtinClassTypes:{builtinClassTypes.Count} diffType:{diffType.Count}");
-        EditorInterface.Singleton.RestartEditor(true);
+    private static string CreateTempDirectory()
+    {
+        var tempPath = Path.GetTempFileName();
+        File.Delete(tempPath);
+        Directory.CreateDirectory(tempPath);
+        return tempPath;
     }
 }
 #endif
